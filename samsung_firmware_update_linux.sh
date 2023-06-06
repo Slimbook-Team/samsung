08884b06f4b1f8fa8b99a8c35b4d15bb861347f8
Slimbook <dev@slimbook.es>
2023-06-06 14:32:32 +0200

    Update samsung_firmware_update_linux.sh

--- samsung_firmware_update_linux.sh
+++ samsung_firmware_update_linux.sh
@@ -121,11 +121,11 @@ while IFS= read -r -d '' archivo; do
   archivos+=("$archivo")
 done < <(find "$directorio/firmware" -maxdepth 1 -type f -print0)
 
-echo "Archivos en el directorio:"
+echo "Elige un archivo :"
 for i in "${!archivos[@]}"; do
   echo "$((i+1)). ${archivos[$i]}"
 done
-read -p "Elige un número de archivo: " opcion
+read -p "Arriba tienes una lista de archivos de firmware en lineas que empiezan por un número, escribe el numero que se llama como tu disco duro con guines bajos. Por ejemplo para un si tienes uns Samsung SSD 840 EVO, marca el número 1: " opcion
 
 # aplicar cambios
 if [[ $opcion =~ ^[0-9]+$ && $opcion -gt 0 && $opcion -le ${#archivos[@]} ]]; then
